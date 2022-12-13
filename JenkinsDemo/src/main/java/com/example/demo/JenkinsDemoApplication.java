d37b736c9ba5a1b99fa70437d83e0e972c058354
ajusha <vlajusha@gmail.com>
2022-12-13 14:34:05 +0530

    first commit

---
+++ JenkinsDemo/src/main/java/com/example/demo/JenkinsDemoApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDemoApplication {
+
+	public static Logger logger = LoggerFactory.getLogger(JenkinsDemoApplication.class);
+
+	public static void main(String[] args) {
+		logger.info("App executing..");
+		SpringApplication.run(JenkinsDemoApplication.class, args);
+	}
+
+}
